51c3c6f7b47b018fee45e045e1760f545ec9ece8
Tamburasca <rtimmermann@astro.uni-bonn.de>
2024-12-05 22:28:41 +0100

    version 0.0.4 - preliminary

--- ecmwf-opendata/src/forecast_viewer.py
+++ ecmwf-opendata/src/forecast_viewer.py
@@ -6,13 +6,56 @@ forecast.json
 """
 
 import os
+import signal
 from datetime import datetime
 import matplotlib.pyplot as plt
-from matplotlib.backend_bases import KeyEvent
+from matplotlib.backend_bases import KeyEvent, PickEvent, MouseButton, MouseEvent
+from matplotlib.figure import Figure
 import json
 
 # data directory relative to source
-DATA_DIR = "{}/../data".format(os.path.dirname(os.path.realpath(__file__)))
+DATA_DIR = "{}/../data".format(
+    os.path.dirname(
+        os.path.realpath(__file__)))
+PICKRADIUS = 5  # Points (Pt). How close the click needs to be to trigger an event.
+
+
+class Onpick(object):
+    def __init__(
+            self,
+            fig: Figure,
+            map_legend_to_ax: dict
+    ):
+        self.fig = fig
+        self.map_legend_to_ax = map_legend_to_ax
+
+    def onpick(
+            self,
+            event: PickEvent
+    ) -> None:
+        legend_line = event.artist
+        # Do nothing if the source of the event is not a legend line.
+        if legend_line not in self.map_legend_to_ax:
+            return
+        ax_line = self.map_legend_to_ax[legend_line]
+        visible = not ax_line.get_visible()
+        ax_line.set_visible(visible)
+        # Change the alpha on the line in the legend, so we can see what lines
+        # have been toggled.
+        legend_line.set_alpha(1.0 if visible else 0.2)
+        self.fig.canvas.draw()
+
+    def invert(self,
+               event: MouseEvent
+               ) -> None:
+        if event.button is MouseButton.RIGHT:
+            for legend_line, ax_line in self.map_legend_to_ax.items():
+                visible = not ax_line.get_visible()
+                ax_line.set_visible(visible)
+                # Change the alpha on the line in the legend, so we can see
+                # what lines have been toggled.
+                legend_line.set_alpha(1.0 if visible else 0.2)
+                self.fig.canvas.draw()
 
 
 def press_key(event: KeyEvent) -> None:
@@ -30,40 +73,75 @@ def read_log() -> dict:
 
 
 def main() -> None:
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
     dict_x = read_log()
     dict_fig: dict = {}
 
     for issue_date, forecast in dict_x.items():
         for item, values in forecast.items():
 
-            if not dict_fig.get(item):
-                fig, ax = plt.subplots()
-                dict_fig[item] = {'fig': fig,
-                                  'ax': ax,
-                                  'lines': list()}
-
             print("Issue Date: {}, Parameter: {}".format(issue_date, item))
             converted_dates = [
                 datetime.strptime(i, '%Y%m%d%H%M') for i in values['time']
             ]
-            dict_fig[item]['ax'].set_title(item)
-            dict_fig[item]['ax'].set_ylabel(values['unit'])
-            dict_fig[item]['ax'].set_xlabel("Time")
+
+            if not dict_fig.get(item):
+                fig, ax = plt.subplots()
+                dict_fig[item] = {
+                    "fig": fig,
+                    "ax": ax,
+                    "lines": list(),
+                    "values": values["unit"],
+                    "map_legend_to_ax": {}
+                }
+
             dict_fig[item]['lines'].append(
                 dict_fig[item]['ax'].plot(
                     converted_dates,
                     values['value'],
                     label=issue_date[:-2]
                 )[0])
-            dict_fig[item]['fig'].canvas.mpl_connect('key_press_event', press_key)
-            dict_fig[item]['ax'].legend(
-                loc='upper left',
-                bbox_to_anchor=(1, 1),
-                fancybox = True,
-                shadow = True
-            )
 
-    print("\nClose all figures by pressing key '1'")
+    for item in dict_fig.keys():
+        dict_fig[item]['ax'].set_title(item)
+        dict_fig[item]['ax'].set_ylabel(dict_fig[item]['values'])
+        dict_fig[item]['ax'].set_xlabel("Time")
+        dict_fig[item]['leg'] = dict_fig[item]['ax'].legend(
+            loc='upper left',
+            bbox_to_anchor=(1, 1),
+            fancybox=True,
+            shadow=True
+        )
+        for legend_line, ax_line in zip(
+                dict_fig[item]['leg'].get_lines(),
+                dict_fig[item]['lines']):
+            legend_line.set_picker(PICKRADIUS)  # Enable picking on the legend line.
+            dict_fig[item]['map_legend_to_ax'][legend_line] = ax_line
+
+        dict_fig[item]['on_pick'] = Onpick(
+            dict_fig[item]['fig'],
+            dict_fig[item]['map_legend_to_ax']
+        )
+
+        dict_fig[item]['leg'].set_draggable(True)
+        dict_fig[item]['fig'].canvas.mpl_connect(
+            'key_press_event',
+            press_key
+        )
+        dict_fig[item]['fig'].canvas.mpl_connect(
+            'pick_event',
+            dict_fig[item]['on_pick'].onpick  # call method of Onpick
+        )
+        dict_fig[item]['fig'].canvas.mpl_connect(
+            'button_press_event',
+            dict_fig[item]['on_pick'].invert  # call method of Onpick
+        )
+
+    print("\nClose all figures by pressing key '1'."
+          "\nClick right mouse button to toggle visibilities of all lines."
+          "\nClick on individual line in legend to toggle its visibility.")
+
     plt.show()
 
 
